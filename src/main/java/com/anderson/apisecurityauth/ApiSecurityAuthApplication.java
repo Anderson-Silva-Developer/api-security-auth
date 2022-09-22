15fec0b60532f44611db892b0c05da0fc81fd776
asc <silvaandersonpi@gmail.com>
2022-09-22 15:20:10 -0300

    commit initial

---
+++ src/main/java/com/anderson/apisecurityauth/ApiSecurityAuthApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.apisecurityauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSecurityAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSecurityAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anderson/apisecurityauth/ApiSecurityAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.apisecurityauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSecurityAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
